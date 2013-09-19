ef3c781429dbad8867f925f4ce351308a0adae0c
John Lim <johnlim.dingwei@gmail.com>
2013-09-19 15:40:22 +0800

    Added source files for Cell which I forgot to add to the repository after refactoring previously.

---
+++ GameOfLife/Cell.h
@@ -0,0 +1,18 @@
+#ifndef _CELL_H_
+#define _CELL_H_
+
+#include <vector>
+
+class Cell{
+public:
+  Cell(int x, int y);
+  bool operator== (const Cell& _cell) const;
+  /*Note: operator== is not used by std::set. Elements a and b are considered equal if !(a < b) && !(b < a)*/
+  bool operator< (const Cell& _cell) const;
+  std::vector<Cell> neighbours() const;
+private:
+  int xCoordinate;
+  int yCoordinate;
+};
+
+#endif
